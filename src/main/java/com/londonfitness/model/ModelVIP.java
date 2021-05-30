2f54ecf00aaa29d17b94ee5edb808d5ae0a4b854
dragonfly keeper <2018213104@bupt.edu.cn>
2021-05-30 16:12:35 +0800

    by dragonfly keeper

--- src/main/java/com/londonfitness/model/ModelVIP.java
+++ src/main/java/com/londonfitness/model/ModelVIP.java
@@ -1,5 +1,28 @@
 package com.londonfitness.model;
 
-public class ModelVIP extends ModelTrainee{
+import com.londonfitness.simDAO.memStorage.Storage;
+import com.londonfitness.simDAO.table.Booking;
+import com.londonfitness.simDAO.table.persons.Trainee;
+
+import java.math.BigDecimal;
+import java.util.Iterator;
+
+public class ModelVIP{
+    private Iterator<Trainee> price;
+    Storage storage;
+    int flag;
+    BigDecimal total = new BigDecimal(0);
+    public ModelVIP(Trainee trainee){
+        for(Iterator<Booking>it = trainee.index_booking.getTnIter(); it.hasNext();){
+            total.add(it.next().externKey_lfClass.raw.price);
+        }
+        int a = total.compareTo(BigDecimal.valueOf(500));
+        if(a==1||a==0){
+            flag = 1;
+        }
+        else{
+            flag = 0;
+        }
+    }
 
 }
