5a10c536a136cb1c42929fbfc5df0f2992ae22fe
dragonfly keeper <2018213104@bupt.edu.cn>
2021-05-30 16:12:35 +0800

    by dragonfly keeper

--- src/main/java/com/londonfitness/model/ModelTrainee.java
+++ src/main/java/com/londonfitness/model/ModelTrainee.java
@@ -1,4 +1,33 @@
 package com.londonfitness.model;
 
+import com.londonfitness.simDAO.table.persons.Coach;
+import com.londonfitness.simDAO.table.persons.Trainee;
+
 public class ModelTrainee {
+    Trainee trainee;
+    ModelVIP vip;
+    public ModelTrainee() {
+
+    }
+
+    public ModelTrainee(Trainee trainee) {
+        this.trainee = trainee;
+        this.vip = new ModelVIP(trainee);
+    }
+
+    public Trainee createTrainee(String... args) {
+        return null;
+    }
+
+    public void deleteTrainee(Trainee trainee) {
+
+    }
+
+    public void updateTrainee(String... args) {
+
+    }
+
+    public ModelVIP getVipState() {
+        return vip;
+    }
 }
