18219317c133607d5da23b2d1558ed19470f0685
YAN <stevenyanmu@outlook.com>
2021-05-31 17:41:00 +0800

    Refine APP launcher

--- src/main/java/com/londonfitness/App.java
+++ src/main/java/com/londonfitness/App.java
@@ -10,47 +10,38 @@ import javax.swing.*;
  *
  */
 public class App extends AppSkeleton {
+    public App() {
+        super(true, true, true, true, false);
+    }
+
     @Override
-    public void bringUpGUI() {
+    public JFrame bringUpGUI() {
         // do service
-        {
-            // prepare
-            // main frame kick in
-            /* Set the Default look and feel */
-            //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-            try {
-                for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                    if ("Default".equals(info.getName())) {
-                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                        break;
-                    }
+
+        // prepare
+        // main frame kick in
+        /* Set the Default look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Default".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
                 }
-            } catch (ClassNotFoundException
-                    | UnsupportedLookAndFeelException
-                    | IllegalAccessException
-                    | InstantiationException ex) {
-                java.util.logging.Logger.getLogger(ServicFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
             }
-            //</editor-fold>
-
-            /* Create and display the form */
-            java.awt.EventQueue.invokeLater(() -> new ServicFrame(getStorage()).setVisible(true));
+        } catch (ClassNotFoundException
+                | UnsupportedLookAndFeelException
+                | IllegalAccessException
+                | InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ServicFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-
-        // storage tables to new xml file, if all done, change name
-        /*
-        display a wait-for-a-while window
-        gen DOM
-        write
-        change name
-        */
-        /*
-        {
-            System.out.println("Start to save data file.");
-            new WriteXML(xdb.documentBuilder, Paths.get(defaultFileName), storage).translate();
-            System.out.println("Data file saved.");
-        }
-         */
+        //</editor-fold>
+        JFrame jf = new ServicFrame(getStorage());
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() ->
+                jf.setVisible(true)
+        );
+        return jf;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/londonfitness/AppSkeleton.java
+++ src/main/java/com/londonfitness/AppSkeleton.java
@@ -1,13 +1,17 @@
 package com.londonfitness;
 
-import com.londonfitness.GUI.ServicFrame;
 import com.londonfitness.simDAO.fileIO.FileScan.ScanXML;
 import com.londonfitness.simDAO.fileIO.XMLDocumentBuilder;
+import com.londonfitness.simDAO.fileIO.xmlwriter.WriteXML;
 import com.londonfitness.simDAO.indexBuilder.RebuildIndexes;
 import com.londonfitness.simDAO.memStorage.Storage;
 
 import javax.swing.*;
 import javax.xml.parsers.ParserConfigurationException;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -22,26 +26,92 @@ public abstract class AppSkeleton {
     private static final String defaultFileName = resourcePathName + "data.xml";
 
     protected Storage storage;
+    private String lastSaved = null;
+    private JFrame jf;
 
-    public AppSkeleton() {
+    public AppSkeleton(boolean load, boolean index, boolean buildGUI, boolean autosave, boolean replaceData) {
         storage = new Storage();
         try {
             XMLDocumentBuilder xdb = new XMLDocumentBuilder();
 
             // from xml, load tables into our container
-            {
+            if (load) {
                 System.out.println("Start to scan data file.");
                 new ScanXML(xdb.documentBuilder, storage, resourcePathName, defaultFileName).scan();
                 System.out.println("Finish scanning data file.");
             }
 
             // from storage, build up indexes
-            {
+            if (index) {
                 System.out.println("Start to build up indexes.");
                 new RebuildIndexes(storage);
                 System.out.println("Finish building indexes.");
             }
-            bringUpGUI();
+
+            if (buildGUI) {
+                jf = bringUpGUI();
+                jf.addWindowListener(new WindowListener() {
+                    @Override
+                    public void windowOpened(WindowEvent e) {
+
+                    }
+
+                    @Override
+                    public void windowClosing(WindowEvent e) {
+                        if (autosave) {
+                            lastSaved = "cache\\save" + OurDateFormat.fileDate.format(new Date()) + ".xml";
+                            System.out.println("Start to save data file.\nFile will be saved to \"" + lastSaved + '\"');
+
+                            new WriteXML(
+                                    xdb.documentBuilder,
+                                    Paths.get(lastSaved),
+                                    storage
+                            ).translate();
+                            System.out.println("Data file saved.");
+                        }
+
+                        if(replaceData) {
+                            // move file to data directory and rename
+                            System.out.println("Replacing main data file with latest save.");
+                            if(lastSaved != null) {
+                                if(Paths.get(lastSaved).toFile()
+                                        .renameTo(Paths.get(defaultFileName).toFile())) {
+                                    System.out.println("Now the data is newest");
+                                } else {
+                                    System.out.println("Move file failed.");
+                                }
+                            } else {
+                                System.out.println("Currently no save");
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void windowClosed(WindowEvent e) {
+
+                    }
+
+                    @Override
+                    public void windowIconified(WindowEvent e) {
+
+                    }
+
+                    @Override
+                    public void windowDeiconified(WindowEvent e) {
+
+                    }
+
+                    @Override
+                    public void windowActivated(WindowEvent e) {
+
+                    }
+
+                    @Override
+                    public void windowDeactivated(WindowEvent e) {
+
+                    }
+                });
+            }
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
         }
@@ -51,5 +121,5 @@ public abstract class AppSkeleton {
         return storage;
     }
 
-    public abstract void bringUpGUI();
+    public abstract JFrame bringUpGUI();
 }
